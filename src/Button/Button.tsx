a7fc384ded3decf01b1535473f3549fc7022d9b0
TheColdPot <guohw979@outlook.com>
2021-01-31 22:47:31 +0800

    feat: add onclick to Button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 interface ButtonProps {
     appearance: 'default' | 'primary',
     prefix: React.ReactNode,
+    onClick: React.EventHandler<any>
     children: React.ReactNode
 }
 
 function Button(props: ButtonProps): React.ReactNode {
-    const { appearance, prefix, children } = props
+    const { appearance, prefix, onClick, children } = props
     const className = 'pot-btn'
         + ' ' + appearance
 
     return (
-        <button className={ className }>
+        <button className={ className } onClick={ onClick }>
             <span className="pot-btn-prefix">{ prefix }</span>
             <span className="pot-btn-text">{ children }</span>
         </button>
@@ -22,11 +23,13 @@ function Button(props: ButtonProps): React.ReactNode {
 
 Button.propTypes = {
     appearance: PropTypes.oneOf(['default', 'primary']),
+    onClick: PropTypes.func,
     prefix: PropTypes.any
 }
 
 Button.defaultProps = {
-    appearance: 'default'
+    appearance: 'default',
+    onClick: () => {}
 }
 
 export default Button;
