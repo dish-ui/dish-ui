134bd5a5a7329d9f262a8435d33217dc1ed3a082
TheColdPot <guohw979@outlook.com>
2021-01-31 09:07:39 +0800

    build: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+module.exports = {
+    entry: './src',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'index.js'
+    },
+	resolve: {
+		extensions: [
+			'.ts',
+			'.tsx',
+			'.js',
+			'.jsx',
+			'.scss',
+			'.less'
+		]
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.[jt]sx?$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-react',
+							'@babel/preset-typescript'
+						],
+						plugins: [
+							'@babel/plugin-proposal-class-properties'
+						]
+					}
+				}
+			}
+		]
+	}
+}
