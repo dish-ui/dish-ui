d5d4b415db1ab09f6dc89a69bd91937aaabc8a08
TheColdPot <guohw979@outlook.com>
2021-02-11 16:44:42 +0800

    feat: add danger button property

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import ComponentProps from '../utils/ComponentProps';
 
 interface ButtonProps extends ComponentProps {
-    primary: boolean
+    primary: boolean,
+    danger: boolean
 }
 function Button(props: ButtonProps) {
-    const { children, primary } = props
+    const { children, primary, danger } = props
     let classNameSuffix: string;
     if (primary) classNameSuffix = '-primary'
+    if (danger) classNameSuffix = '-danger'
     return (
         <div className={ 'pot-button' + classNameSuffix }>
             { children }
@@ -18,6 +20,11 @@ function Button(props: ButtonProps) {
 
 Button.propTypes = {
     primary: PropTypes.bool
+}
+
+Button.defaultProps = {
+    primary: false,
+    danger: false
 }
 
 export default Button;
