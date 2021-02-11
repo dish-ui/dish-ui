213f03d2b4c4f628e816d76e524ca74cfdc2c110
TheColdPot <guohw979@outlook.com>
2021-02-11 14:46:24 +0800

    feat: delete all files

--- src/Button/index.tsx
+++
@@ -1,3 +0,0 @@
-import Button from './Button';
-import './style.scss'
-export default Button;
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import Button from './Button';
-import Font from './Font';
-
-import './styles/general.scss'
-
-export {
-	Button,
-    Font
-}
