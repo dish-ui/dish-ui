578d50f603d7234b6c4ef83c046207b164b2748c
TheColdPot <guohw979@outlook.com>
2021-02-14 15:58:38 +0800

    refactor: change ButtonFooter className to right style

--- src/Button/ButtonFooter.tsx
+++ src/Button/ButtonFooter.tsx
@@ -3,7 +3,7 @@ import IComponentProps from '../utils/IComponentProps';
 
 function ButtonFooter(props: IComponentProps): React.ReactElement {
     const { children } = props;
-    return <div className="pot-button-footer">{children}</div>;
+    return <div className="pot-ButtonFooter">{children}</div>;
 }
 
 export default ButtonFooter;
